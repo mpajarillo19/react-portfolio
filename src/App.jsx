becf7a17750f6978f3cc00cbd76d6579f4bb92df
Migs <migspajarillo19@gmail.com>
2022-04-03 14:26:26 +0800

    Removed Unwanted components

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
       <Experience />
       <Services />
       <Portfolio />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
